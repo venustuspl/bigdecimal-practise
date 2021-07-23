eb9ad6d2313adabb5106509b6ae6d5f56c2fb6bc
venustuspl <venustus.pl@gmail.com>
2021-07-23 21:11:52 +0200

    feature: BigDecimal precision example

--- src/main/java/BigDecimalEx.java
+++ src/main/java/BigDecimalEx.java
@@ -3,7 +3,7 @@ import java.math.RoundingMode;
 
 public class BigDecimalEx {
     public static void main(String[] args) {
-
+        // simple operations
         var val1 = new BigDecimal("3.44");
         var val2 = new BigDecimal("2.74");
 
@@ -21,5 +21,21 @@ public class BigDecimalEx {
 
         BigDecimal res5 = val1.divide(val2, 4, RoundingMode.DOWN);
         System.out.println(res5);
+
+        //precision
+        double a = 0.1 + 0.1 + 0.1;
+        double b = 0.3;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a == b);
+
+        var c = new BigDecimal("0.1").add(new BigDecimal("0.1"))
+                .add(new BigDecimal("0.1"));
+        var d = new BigDecimal("0.3");
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(c.equals(d));
     }
 }
